2b635adf38a46ef71f43c9589781833a3082a9c8
Paula Ponce <paula.poncez@usach.cl>
2017-07-23 02:18:11 -0400

    header styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,24 @@
 $(document).ready(function() {
+	//Dropdown
 	$('ul.nav li.dropdown').hover(function() {
 		$(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(200);
 	}, function() {
 		$(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(200);
 	});
+	//Scroll down navbar
+	var scrollStart = 0;
+	var startChange = $("#carousel h1");
+	var offset = startChange.offset();
+	if (startChange.length) {
+		$(document).scroll(function() {
+		scrollStart = $(this).scrollTop();
+			if (scrollStart > offset.top) {
+				$("#navbar").addClass("nav-white");
+				$("#navbar").removeClass("nav-transparent");
+			} else {
+				$("#navbar").removeClass("nav-white");
+				$("#navbar").addClass("nav-transparent");
+			}
+		});
+	}	
 });
